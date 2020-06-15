c05e41d444927ae7e4a287252ecbd4e0990480bd
Saubhik Singh <saubhik.singh01@gmail.com>
2020-06-15 22:08:24 +0530

    fixed small req bug

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import { GraphQLServer } from "graphql-yoga"
 import dotenv from "dotenv";
-import morgan from "morgan";
 
 import typeDefs from "./src/schema/index";
 import resolvers from "./src/resolvers/index";
@@ -10,22 +9,21 @@ const PORT = process.env.PORT || 4000;
 
 dotenv.config();
 
-const logInput = async (resolve, root, args, context, info) => {
-  console.log(`1. logInput: ${JSON.stringify(args)}`)
-  const result = await resolve(root, args, context, info)
-  return result
-}
 
 const server = new GraphQLServer({
   typeDefs,
   resolvers,
   cors: true,
-  context: req => req.request.headers
+  context: req => req.request
 });
 
 connectDatabase()
   .then(() => {
-    server.start({ cors: true, port: PORT }, () => console.log(`Listening on port ${PORT}`)
+    server.start({
+      cors: true,
+      port: PORT
+    },
+      () => console.log(`Listening on port ${PORT}`)
     )
   })
   .catch((error) =>
